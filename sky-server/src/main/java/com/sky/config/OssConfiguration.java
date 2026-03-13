8d8312299d006c61a82ef4da44154ac94a755608
ZamirZhang <1258858403@qq.com>
2026-03-14 00:02:52 +0800

    feat: 创建 OSS 配置类

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,23 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 阿里云文件上传配置类，用来生成 AliOssProperties 对象并交由 IOC 容器管理
+ */
+@Configuration
+public class OssConfiguration {
+    @Bean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        return new AliOssUtil(
+                aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName()
+        );
+    }
+}
